043fd16231481662aa01904402787a070354c18e
havababay <hava.babay@gmail.com>
2021-01-05 20:59:41 +0200

    2 files modified, 2 added, 1 deleted

---
+++ src/app/obstacles.ts
@@ -0,0 +1,14 @@
+export const obstacles = [
+  {
+    id: 1,
+    name: 'Jumping Spider',
+  },
+  {
+    id: 2,
+    name: 'Balance Bridge',
+  },
+  {
+    id: 3,
+    name: 'Warped Wall',
+  }
+];
---
+++ src/app/shows.ts
@@ -0,0 +1,12 @@
+export const shows = [
+  {
+    id: 1,
+    obstacles: [1,2,3],
+    candidates: [1,2,3]
+  },
+  {
+    id: 2,
+    obstacles: [1,2,4],
+    candidates: [1,2,4]
+  },
+];
